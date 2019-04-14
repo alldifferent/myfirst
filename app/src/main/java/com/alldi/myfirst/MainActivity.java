285318ed17acad1fc40294ce81746f00dc269098
alldifferent <alldifferent@naver.com>
2019-04-14 12:23:47 +0900

    연습문제 - Toast에 ID출력

---
+++ app/src/main/java/com/alldi/myfirst/MainActivity.java
@@ -0,0 +1,43 @@
+package com.alldi.myfirst;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText userIdEdt;
+    EditText passwordEdt;
+    Button loginBtn;
+    TextView wasaviTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        userIdEdt = findViewById(R.id.userIdEdt);
+        passwordEdt = findViewById(R.id.passwordEdt);
+        loginBtn = findViewById(R.id.loginBtn);
+        wasaviTxt = findViewById(R.id.wasaviTxt);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userId = userIdEdt.getText().toString();
+                String password = passwordEdt.getText().toString();
+
+                Toast.makeText(MainActivity.this, "ID : "+ userId, Toast.LENGTH_SHORT).show();
+
+                wasaviTxt.setText("ID:"+userId+" Password:"+password);
+
+
+
+            }
+        });
+    }
+}
